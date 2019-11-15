c7c80e4ba0d4fe462b63d458939ea9dbf061cf70
Loredana Cosma <lorevonelbeland@gmail.com>
2019-11-15 14:40:06 +0100

    changes

--- app/src/main/java/com/example/aiplant/create_profile/PlantProfile.java
+++ app/src/main/java/com/example/aiplant/create_profile/PlantProfile.java
@@ -1,108 +1,86 @@
 package com.example.aiplant.create_profile;
 
-import android.Manifest;
-import android.app.DatePickerDialog;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.Color;
-import android.os.Build;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
+import android.os.Parcel;
+import android.os.Parcelable;
 
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import java.io.Serializable;
+import java.util.InputMismatchException;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
-import com.bumptech.glide.Glide;
-import com.example.aiplant.R;
-import com.example.aiplant.cameraandgallery.ImagePicker;
-
-import java.io.IOException;
-import java.util.Calendar;
-
-public class PlantProfile extends AppCompatActivity implements View.OnClickListener {
-
-    private static final String TAG = "PlantProfile";
-    static final int REQUEST_CODE = 123;
-    private ImageView profilePicture;
-    private LinearLayout birthdayLayout;
-    private EditText minHumidity, maxHumidity, minSunlight ,maxSunlight,
-                     minTemperature ,maxTemperature, bd_year, bd_month, bd_day, namePlant;
-    private Bitmap picture;
-    private ImageButton createProfile;
-    private String[] permissions = {Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE};
-    private final int REQUEST_GALLERY=2;
-    private final int REQUEST_CAMERA=1;
+public class PlantProfile implements Serializable, Parcelable {
 
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_new_plant_profile);
-       initializeView();
-
-        profilePicture.setOnClickListener(v -> {
-            Intent chooseImageIntent = ImagePicker.getPickImageIntent( getApplicationContext() );
-            startActivityForResult( chooseImageIntent, REQUEST_CODE );
-            checkPermissions();
-        });
-
-        createProfile.setOnClickListener(v -> {
-            if(namePlant.getText().toString().equals("")) {
-                namePlant.setError(getString(R.string.error_name));
-                namePlant.requestFocus();
-            }
-        });
-
-
-    }
-
-    private void initializeView() {
-        profilePicture = findViewById(R.id.profilePicPlant_imgView);
-        namePlant = findViewById(R.id.namePlant_editText);
-        bd_day = findViewById(R.id.bd_day);
-        bd_month = findViewById(R.id.bd_month);
-        bd_year = findViewById(R.id.bd_year);
-        birthdayLayout = findViewById(R.id.birthday_layout);
-        minHumidity = findViewById(R.id.minHumidity_editText);
-        maxHumidity = findViewById(R.id.maxHumidity_editText);
-        minSunlight = findViewById(R.id.minSunlight_editText);
-        maxSunlight = findViewById(R.id.maxSunlight_editText);
-        minTemperature = findViewById(R.id.minTemperature_editText);
-        maxTemperature = findViewById(R.id.maxTemperature_editText);
-        createProfile = findViewById(R.id.createProfile_btn);
-    }
-
-    private void checkPermissions() {
-        if (Build.VERSION.SDK_INT >= 23) {
-            ActivityCompat.requestPermissions(this, permissions, REQUEST_GALLERY | REQUEST_CAMERA);
+    public static final Creator<PlantProfile> CREATOR = new Creator<PlantProfile>() {
+        @Override
+        public PlantProfile createFromParcel(Parcel in) {
+            return new PlantProfile(in);
         }
+
+        @Override
+        public PlantProfile[] newArray(int size) {
+            return new PlantProfile[size];
+        }
+    };
+
+    private String name;
+    private String userID;
+
+    public PlantProfile (String userID, String name){
+        this.userID=userID;
+        this.name=name;
+    }
+
+    public PlantProfile(){
+
+    }
+
+    protected PlantProfile(Parcel in){
+        userID = in.readString();
+        name = in.readString();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+
+    @Override
+    public int describeContents() {
+        return 0;
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        Log.d( TAG, "request code: " + requestCode );
-        Log.d( TAG, "result code: " + resultCode );
-        if ( resultCode == RESULT_OK && requestCode == REQUEST_CODE ) {
-            Glide.with(this).load(data.getData()).fitCenter().into(profilePicture);
-
-        } else
-            Log.d( TAG, "Nothing selected!" );
-
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeString(userID);
     }
 
-    @Override
-    public void onClick(View view) {
+    public static class Builder{
+        private String sUserID;
+        private String sName;
 
+        public Builder() {}
+
+        public Builder withName(String name){
+            if(name.length() == 0) {
+                throw new InputMismatchException("No name");
+            }
+            sName = name;
+            return this;
+        }
+
+        public PlantProfile build(){
+            PlantProfile profile = new PlantProfile();
+            profile.userID = sUserID;
+            profile.name = sName;
+            return profile;
+        }
     }
 }
---
+++ app/src/main/java/com/example/aiplant/device_connection/DeviceConnectionActivity.java
@@ -0,0 +1,234 @@
+package com.example.aiplant.device_connection;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.example.aiplant.R;
+import com.example.aiplant.home.HomeActivity;
+import com.example.aiplant.welcome.PrefManager;
+
+public class DeviceConnectionActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private LinearLayout dotsLayout;
+    private int[] layouts;
+    private Button  btnNext;
+    private Button connectDevice;
+    private LinearLayout connectionSuccessful;
+    private ProgressBar loadingCircle;
+    private PrefManager prefManager;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Checking for first time launch - before calling setContentView()
+        prefManager = new PrefManager(this);
+        if (!prefManager.isFirstTimeLaunch()) {
+            launchHomeScreen();
+            finish();
+        }
+
+        // Making notification bar transparent
+        if (Build.VERSION.SDK_INT >= 21) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
+
+        setContentView(R.layout.activity_device_connection);
+
+        viewPager =  findViewById(R.id.view_pager);
+        dotsLayout = findViewById(R.id.layoutDots);
+        btnNext = findViewById(R.id.btn_next);
+        connectDevice = findViewById(R.id.connect_device_btn);
+        connectionSuccessful = findViewById(R.id.check_sign_layout);
+        loadingCircle = findViewById(R.id.loading_circle);
+
+        connectionSuccessful.setVisibility(View.GONE);
+        loadingCircle.setVisibility(View.INVISIBLE);
+        connectDevice.setVisibility(View.INVISIBLE);
+
+
+
+
+        // layouts of all welcome sliders
+        layouts = new int[]{
+                R.layout.fragment_one_connect,
+                R.layout.fragment_two_connect,
+                R.layout.fragment_three_connect,
+                };
+
+        // adding bottom dots
+        addBottomDots(0);
+
+        // making notification bar transparent
+        changeStatusBarColor();
+
+        MyViewPagerAdapter myViewPagerAdapter = new MyViewPagerAdapter();
+        viewPager.setAdapter(myViewPagerAdapter);
+        viewPager.addOnPageChangeListener(viewPagerPageChangeListener);
+
+      //  btnSkip.setOnClickListener(v -> launchHomeScreen());
+
+        btnNext.setOnClickListener(v -> {
+            // checking for last page
+            // if last page home screen will be launched
+            int current = getItem(+1);
+            if (current < layouts.length) {
+                // move to next screen
+                viewPager.setCurrentItem(current);
+            } else {
+                launchHomeScreen();
+            }
+
+
+
+
+
+        });
+    }
+
+    private void addBottomDots(int currentPage) {
+        TextView[] dots = new TextView[layouts.length];
+
+        int[] colorsActive = getResources().getIntArray(R.array.array_dot_active_connect);
+        int[] colorsInactive = getResources().getIntArray(R.array.array_dot_inactive_connect);
+
+        dotsLayout.removeAllViews();
+        for (int i = 0; i < dots.length; i++) {
+            dots[i] = new TextView(this);
+            dots[i].setText(Html.fromHtml("&#8226;"));
+            dots[i].setTextSize(35);
+            dots[i].setTextColor(colorsInactive[currentPage]);
+            dotsLayout.addView(dots[i]);
+        }
+
+        if (dots.length > 0)
+            dots[currentPage].setTextColor(colorsActive[currentPage]);
+    }
+
+    private int getItem(int i) {
+        return viewPager.getCurrentItem() + i;
+    }
+
+    private void launchHomeScreen() {
+        prefManager.setFirstTimeLaunch(false);
+        startActivity(new Intent(DeviceConnectionActivity.this, HomeActivity.class));
+        finish();
+    }
+
+    //  viewpager change listener
+    ViewPager.OnPageChangeListener viewPagerPageChangeListener = new ViewPager.OnPageChangeListener() {
+
+        @Override
+        public void onPageSelected(int position) {
+            addBottomDots(position);
+
+            // changing the next button text 'NEXT' / 'GOT IT'
+            if (position == layouts.length - 1) {
+                // last page. make button text to GOT IT
+                btnNext.setText(getString(R.string.start));
+              //  btnSkip.setVisibility(View.GONE);
+            } else {
+                // still pages are left
+                btnNext.setText(getString(R.string.next));
+//                btnSkip.setVisibility(View.VISIBLE);
+            }
+            if(layouts[position]==R.layout.fragment_two_connect){
+                connectDevice.setVisibility(View.VISIBLE);
+
+                connectDevice.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        loadingCircle.setVisibility(View.VISIBLE);
+
+                    }
+                });
+            }
+            else{
+                connectDevice.setVisibility(View.GONE);
+                loadingCircle.setVisibility(View.INVISIBLE);
+                connectionSuccessful.setVisibility(View.GONE);
+            }
+        }
+
+        @Override
+        public void onPageScrolled(int arg0, float arg1, int arg2) {
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int arg0) {
+
+        }
+    };
+
+    /**
+     * Making notification bar transparent
+     */
+    private void changeStatusBarColor() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        }
+    }
+
+
+
+    /**
+     * View pager adapter
+     */
+    public class MyViewPagerAdapter extends PagerAdapter {
+        private LayoutInflater layoutInflater;
+
+        public MyViewPagerAdapter() {
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            View view = layoutInflater.inflate(layouts[position], container, false);
+            container.addView(view);
+
+            return view;
+        }
+
+        @Override
+        public int getCount() {
+            return layouts.length;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object obj) {
+            return view == obj;
+        }
+
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            View view = (View) object;
+            container.removeView(view);
+        }
+    }
+
+}
