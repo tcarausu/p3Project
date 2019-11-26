6fbe18a1d8f595fcd42325b5ad36a2576daf9a31
LoreVonElbeland <44893161+LoreVonElbeland@users.noreply.github.com>
2019-11-26 11:52:36 +0100

    plant profile creation - done (#15)

--- app/src/main/java/com/example/aiplant/cameraandgallery/ImagePicker.java
+++ app/src/main/java/com/example/aiplant/cameraandgallery/ImagePicker.java
@@ -1,24 +1,18 @@
 package com.example.aiplant.cameraandgallery;
 
-import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ResolveInfo;
 import android.content.res.AssetFileDescriptor;
-import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Matrix;
-import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Parcelable;
 import android.os.StrictMode;
 import android.provider.MediaStore;
 import android.util.Log;
-
-import androidx.core.app.ActivityCompat;
 
 import com.example.aiplant.R;
 
@@ -28,7 +22,7 @@ import java.io.IOException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
+
 
 /**
  * Class that allows the user to choose a picture form gallery or open a new phone camera intent.
@@ -116,15 +110,6 @@ public class ImagePicker {
             Log.d(TAG, "selectedImage: " + selectedImage);
 
             bm = getImageResized(context, selectedImage);
-            int rotation = getRotation(context, selectedImage, isCamera);
-            if (bm.getHeight() >= bm.getWidth()) {
-                if (isCamera) {
-                    bm = rotate(bm, rotation + 180);
-                } else bm = rotate(bm, rotation);
-            } else {
-
-                bm = rotate(bm, rotation + 90);
-            }
 
         }
         return bm;
@@ -174,78 +159,6 @@ public class ImagePicker {
             Log.d(TAG, "resizer: new bitmap width = " + bm.getWidth());
             i++;
         } while (bm.getWidth() < DEFAULT_MIN_WIDTH_QUALITY && i < sampleSizes.length);
-        return bm;
-    }
-
-    /**
-     * This method rotates the pictures
-     *
-     * @param context gets the context of the application
-     * @param imageUri is the Uri reference of the picture
-     * @param isCamera checks if the intent is the camera, else being the gallery
-     * @return
-     */
-    private static int getRotation(Context context, Uri imageUri, boolean isCamera) {
-        int rotation;
-
-        if (isCamera) {
-            rotation = getRotationFromCamera(context, imageUri);
-        } else {
-            rotation = getRotationFromGallery(context, imageUri);
-        }
-        Log.d(TAG, "Image rotation: " + rotation);
-        return rotation;
-    }
-
-    private static int getRotationFromCamera(Context context, Uri imageFile) {
-        int rotate = 0;
-
-        try {
-            context.getContentResolver().notifyChange(imageFile, null);
-            ExifInterface exif = new ExifInterface(Objects.requireNonNull(imageFile.getPath()));
-            int orientation = exif.getAttributeInt(
-                    ExifInterface.TAG_ORIENTATION,
-                    ExifInterface.ORIENTATION_UNDEFINED);
-
-            switch (orientation) {
-                case ExifInterface.ORIENTATION_ROTATE_270:
-                    rotate = 270;
-                    break;
-                case ExifInterface.ORIENTATION_ROTATE_180:
-                    rotate = 180;
-                    break;
-                case ExifInterface.ORIENTATION_ROTATE_90:
-                    rotate = 90;
-                    break;
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return rotate;
-    }
-
-    private static int getRotationFromGallery(Context context, Uri imageUri) {
-        int result = 0;
-        String[] columns = {MediaStore.Images.Media.ORIENTATION};
-        try (Cursor cursor = context.getContentResolver().query(imageUri, columns, null, null, null)) {
-            if (cursor != null && cursor.moveToFirst()) {
-                int orientationColumnIndex = cursor.getColumnIndex(columns[0]);
-                result = cursor.getInt(orientationColumnIndex);
-            }
-        } catch (Exception e) {
-            Log.e(TAG, "getRotationFromGallery exception: ",e );
-            //Do nothing ??    hhehe why not! you can log the exception at least
-        }
-        return result;
-    }
-
-
-    private static Bitmap rotate(Bitmap bm, int rotation) {
-        if (rotation != 0) {
-            Matrix matrix = new Matrix();
-            matrix.postRotate(rotation);
-            return Bitmap.createBitmap(bm, 0, 0, bm.getWidth(), bm.getHeight(), matrix, true);
-        }
         return bm;
     }
 
